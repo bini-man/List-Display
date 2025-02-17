dadcc99bb8e115ac4eb1c08fc846cf4af33feef4
biniyam badege <biniyambadege4@gmail.com>
2025-02-17 12:44:14 +0400

    List Display Application. => .env file create to store the API Adress and .env added in the gitignore file => constants created => custome Hook created for fetching data from the api

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const constants = {
+    drawerWidth: 240,
+    productAPI: process.env.REACT_APP_FAKESTORE_API,
+    title: 'My React App',
+    navigation: ['Inbox', 'Starred', 'Send email', 'Drafts'],
+    errorMessage: 'Error fetching data'
+};
