23156a7d2e3a51f1ea65be43c94d8b805286a012
biniyam badege <biniyambadege4@gmail.com>
2025-02-17 13:12:21 +0400

    List Display Application. => Error.js removed => loading and error handling file merged => API consumed properly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import useFetchProduct from './Hooks/useFetchProduct';
+import LoadingOrError from './components/LoadingOrError';
+import { Box, Typography } from '@mui/material';
 function App() {
+  const { products, isLoading,  error,  isDrawerOpen, handleDrawerOpen, handleDrawerClose } = useFetchProduct();
+  if(isLoading || error) {
+    return <LoadingOrError isLoading={isLoading} error={error} /> 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex' }}>
+        <div>
+           {products.map(product => (
+               <Typography variant="body1" color="text.secondary">{product.title}.</Typography>
+           ))}
+        </div>
+    </Box>
   );
 }
 
